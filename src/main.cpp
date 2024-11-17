57de3e8769b8caaea29e97722a22cffff510e028
22tachibana <tachibana-takatoki357@g.ecc.u-tokyo.ac.jp>
2024-11-17 16:26:13 +0900

    eigenのlink

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
+#include <eigen3/Eigen/Dense>
+
 int main(){
     std::cout << "Hello World!" << std::endl;
+    Eigen::MatrixXd m(2,2);
+    m << 1, 2,
+         3, 4;
+    std::cout << m << std::endl;
     return 0;
 }
